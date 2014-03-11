ede890b9248206e7bbf89ea44d90a9d7665f47ed
Bruno Larouche <bruno.larouche@gmail.com>
2014-03-10 21:59:13 -0400

    Playing two videos back to back.

--- main.rb
+++ main.rb
@@ -1,1 +1,2 @@
-system "omxplayer $(youtube-dl -g \"http://www.youtube.com/watch?v=xIASaUUwklk\")"
+system "omxplayer $(youtube-dl -g \"http://www.youtube.com/watch?v=VvqbjUtt3mM\")"
+system "omxplayer $(youtube-dl -g \"http://www.youtube.com/watch?v=M50wnj5Se4g\")"
