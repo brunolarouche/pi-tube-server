6eff379c8f169928f81a61a89be62943ff745c84
Bruno Larouche <bruno.larouche@gmail.com>
2014-03-15 16:41:08 -0400

    Thread tryouts, round 1.

--- main.rb
+++ main.rb
@@ -3,14 +3,18 @@ require 'highline/import'
 YOUTUBE_URL_PREFIX = "http://www.youtube.com/watch?v="
 
 def download_video url
-  system "youtube-dl -o \"../videos/%(id)s\" #{url}"
+  `youtube-dl -o \"../videos/%(id)s\" #{url}`
 end
 
 def play_youtube_video youtube_id
   youtube_video_path = Pathname.new("../videos/#{youtube_id}")
   download_video YOUTUBE_URL_PREFIX + youtube_id unless youtube_video_path.exist?
-  system "omxplayer ../videos/#{youtube_id}"
+  thomas = `omxplayer ../videos/#{youtube_id}`
+  p thomas
 end
 
 youtube_id = ask('Veuillez saisir l\'identifiant youtube du vidéo que vous désirez démarrer.')
-play_youtube_video youtube_id
+t1=Thread.new{ play_youtube_video youtube_id }
+t2=Thread.new{ sleep(30); `kill #{$?.pid}` }
+t1.join
+t2.join
