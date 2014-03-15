4f2c58af8db1bf5530399cf269cfedfc75e849ac
Bruno Larouche <bruno.larouche@gmail.com>
2014-03-15 15:55:30 -0400

    The app now ask about which video to play.

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'highline/import'
+
 YOUTUBE_URL_PREFIX="http://www.youtube.com/watch?v="
 
 def download_video url
@@ -7,4 +9,8 @@ end
 def play_youtube_video youtube_id
   download_video YOUTUBE_URL_PREFIX + youtube_id unless Pathname.new("../videos/#{youtube_id}").exist?
   system "omxplayer ../videos/#{youtube_id}"
+end
+
+ask "Veuillez saisir l'identifiant youtube du vidéo que vous désirez démarrer." do |youtube_id|
+  play_youtube_video youtube_id
 end
