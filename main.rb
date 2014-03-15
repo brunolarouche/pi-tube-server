a009c5e6539e787071c37c9987e8b53eedee4de9
Bruno Larouche <bruno.larouche@gmail.com>
2014-03-15 17:49:26 -0400

    Old debug code removed.

--- main.rb
+++ main.rb
@@ -9,8 +9,7 @@ end
 def play_youtube_video youtube_id
   youtube_video_path = Pathname.new("../videos/#{youtube_id}")
   download_video YOUTUBE_URL_PREFIX + youtube_id unless youtube_video_path.exist?
-  thomas = `omxplayer ../videos/#{youtube_id}`
-  p thomas
+  `omxplayer ../videos/#{youtube_id}`
 end
 
 loop do
