4a2c7fc21a650eeedc3a099400caaad6ffba312a
Bruno Larouche <bruno.larouche@gmail.com>
2014-03-15 17:51:02 -0400

    Double quote strings were needed, fixed.

--- main.rb
+++ main.rb
@@ -3,13 +3,13 @@ require 'highline/import'
 YOUTUBE_URL_PREFIX = "http://www.youtube.com/watch?v="
 
 def download_video url
-  system 'youtube-dl -o \"../videos/%(id)s\" #{url}'
+  system "youtube-dl -o \"../videos/%(id)s\" #{url}"
 end
 
 def play_youtube_video youtube_id
   youtube_video_path = Pathname.new("../videos/#{youtube_id}")
   download_video YOUTUBE_URL_PREFIX + youtube_id unless youtube_video_path.exist?
-  system 'omxplayer ../videos/#{youtube_id}'
+  system "omxplayer ../videos/#{youtube_id}"
 end
 
 loop do
