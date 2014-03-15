c588355e70c81151d5e687a2792b3a3b59c9f38b
Bruno Larouche <bruno.larouche@gmail.com>
2014-03-15 15:48:15 -0400

    Videos can now be played and downloaded if not present.

--- main.rb
+++ main.rb
@@ -1,2 +1,10 @@
-system "omxplayer $(youtube-dl -g \"http://www.youtube.com/watch?v=VvqbjUtt3mM\")"
-system "omxplayer $(youtube-dl -g \"http://www.youtube.com/watch?v=M50wnj5Se4g\")"
+YOUTUBE_URL_PREFIX="http://www.youtube.com/watch?v="
+
+def download_video url
+  system "youtube-dl -o \"../videos/%(id)s\" #{url}"
+end
+
+def play_youtube_video youtube_id
+  download_video YOUTUBE_URL_PREFIX + youtube_id unless Pathname.new("../videos/#{youtube_id}").exist?
+  system "omxplayer ../videos/#{youtube_id}"
+end
